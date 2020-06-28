c29e69eea77e71f60d6b43635e4f0b901086742c
Shafiq Ahmed <ahmedshaf02@gmail.com>
2020-06-28 18:44:19 +0530

    reservation system

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Page from "./Page";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Page />
+    </div>
+  );
+}
