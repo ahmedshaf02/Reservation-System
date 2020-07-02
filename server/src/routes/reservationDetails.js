ba5dbfe8c94017cda80ad25e81bc7f6f3cd93787
Shafiq Ahmed <ahmedshaf02@gmail.com>
2020-07-03 02:28:16 +0530

    project updated server side

--- server/src/routes/reservationDetails.js
+++ server/src/routes/reservationDetails.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const mongo = require("mongoose");
 const Reservation = mongo.model("Reservation");
 const Booking = mongo.model("Booking");
+const nodemailer = require("nodemailer")
+const sendgridTransport = require("nodemailer-sendgrid-transport")
+
+const transport = nodemailer.createTransport(sendgridTransport({
+    auth:{
+      api_key: "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"
+    }
+}))
 
 router.get("/", (req, res) => {
   res.json("sample text");
@@ -34,6 +42,22 @@ router.post("/tablebook", (req, res) => {
           if(!data){
             res.status(422).json({error:"not booked"})
           }
+
+        transport.sendMail({
+          to:email,
+          from:"noreplyshwarmaclub@gmail.com",
+          subject:"table booking confirmation email",
+          html:`
+              <h2>Thank you for booking with us</h2>
+              <h4>
+              you have booked ${tableName} for ${capacity} peopele on date 
+              ${date} for ${location} location.
+              </h4>
+          `
+        })
+        .then(email=>console.log(email))
+        .catch(err=>console.log(err))
+
           res.json(data)
           console.log(data)
         })
